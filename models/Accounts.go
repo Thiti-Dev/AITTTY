c099a7b0a23de5eb8aa2c28c75cccbd9d40e0a22
Thiti Dev <thiti.mwk.main@gmail.com>
2020-12-31 01:43:29 +0700

    :wrench: [model]: Created accounts model

---
+++ models/Accounts.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// Accounts -> An struct that describe the user-account key field
+type Accounts struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Username  string             `json:"username,omitempty" bson:"username,omitempty" binding:"required"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty" binding:"required"`
+	CreatedAt time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty" binding:"required"`
+	UpdatedAt time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty" binding:"required"`
+}
