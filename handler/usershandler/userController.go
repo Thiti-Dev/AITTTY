342092849cb574f3a0485e7a48b918f86a712e85
Thiti Dev <thiti.mwk.main@gmail.com>
2020-12-31 02:37:42 +0700

    :zap: [handler->user]: (fn)CreateAccount => check if email is already existed

--- handler/usershandler/userController.go
+++ handler/usershandler/userController.go
@@ -9,6 +9,8 @@ import (
 	"github.com/Thiti-Dev/AITTTY/helpers"
 	"github.com/Thiti-Dev/AITTTY/models"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // StatusCheckAPI -> is for checking if the server is properly running at the moment
@@ -34,21 +36,34 @@ func CreateAccount(c *fiber.Ctx) error {
 	if err := c.BodyParser(users); err != nil {
 		return helpers.ResponseMsg(c, 400, err.Error(), nil)
 	} else {
-		//asynchronize task -> passing context to insertion task
-		if r, err := db.Collection("users").InsertOne(ctx, users); err != nil {
-			return helpers.ResponseMsg(c, 500, "Inserted data unsuccesfully", err.Error())
-		} else {
-			// Anonymous struct
-			respData := struct {
-				CreatedID    interface{}
-				Username	string
-				Email 		string
-			}{
-				r.InsertedID,
-				users.Username,
-				users.Email,
+
+		//Check if this email already exist in db or not
+		q := bson.M{"email": users.Email}
+		result := db.Collection("users").FindOne(ctx, q)
+		if result.Err() != nil {
+			// To ensure if document isn't exist
+			if result.Err() == mongo.ErrNoDocuments  { 
+				//asynchronize task -> passing context to insertion task
+				if r, err := db.Collection("users").InsertOne(ctx, users); err != nil {
+					return helpers.ResponseMsg(c, 500, "Inserted data unsuccesfully", err.Error())
+				} else {
+					// Anonymous struct
+					respData := struct {
+						CreatedID    interface{}
+						Username	string
+						Email 		string
+					}{
+						r.InsertedID,
+						users.Username,
+						users.Email,
+					}
+					return helpers.ResponseMsg(c, 200, "Inserted data succesfully", respData)
+				}
+			}else{
+				return helpers.ResponseMsg(c, 400, "Somethings went wrong", result.Err().Error())
 			}
-			return helpers.ResponseMsg(c, 200, "Inserted data succesfully", respData)
+		}else{
+			return helpers.ResponseMsg(c, 400, "This email is already existed", nil) 
 		}
 	}
 }
