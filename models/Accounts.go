4e0c4393f2e6e53c192de4ab56cf0dce730f9a2d
Thiti Dev <thiti.mwk.main@gmail.com>
2021-01-01 01:33:26 +0700

    :wrench: [models->account]: Added tags rule

--- models/Accounts.go
+++ models/Accounts.go
@@ -9,8 +9,8 @@ import (
 // Accounts -> An struct that describe the user-account key field
 type Accounts struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Username  string             `json:"username,omitempty" bson:"username,omitempty" binding:"required"`
-	Email     string             `json:"email,omitempty" bson:"email,omitempty" binding:"required"`
+	Username  string             `json:"username,omitempty" bson:"username,omitempty" binding:"required" validate:"required"`
+	Email     string             `json:"email,omitempty" bson:"email,omitempty" binding:"required" validate:"required,email"`
 	CreatedAt time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty" binding:"required"`
 	UpdatedAt time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty" binding:"required"`
 }
