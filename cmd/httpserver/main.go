d8ef3b26a36680ece51f9d0090d52d3d3c3ff752
Thiti Dev <thiti.mwk.main@gmail.com>
2021-01-01 01:35:50 +0700

    :wrench: [main.go]: Calls InitializeTranslator when the server just've been initialized

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -4,11 +4,18 @@ import (
 	"log"
 
 	"github.com/Thiti-Dev/AITTTY/cmd/httpserver/router"
+	"github.com/Thiti-Dev/AITTTY/helpers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
+
+	//Pkg initializer
+	helpers.InitializeTranslator()
+	// ────────────────────────────────────────────────────────────────────────────────
+
+
 	app := fiber.New()
 	app.Use(logger.New()) // appiled logger middleware
 	router.RoutesSetup(app)
