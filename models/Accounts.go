22668a953c001fa076c4c34761b24995f91b5ef6
Thiti Dev <thiti.mwk.main@gmail.com>
2021-01-01 23:01:48 +0700

    :wrench: [model->account]: Created (struct)AccountsSignIn

--- models/Accounts.go
+++ models/Accounts.go
@@ -15,3 +15,9 @@ type Accounts struct {
 	CreatedAt time.Time          `json:"createdAt,omitempty" bson:"createdAt,omitempty" binding:"required"`
 	UpdatedAt time.Time          `json:"updatedAt,omitempty" bson:"updatedAt,omitempty" binding:"required"`
 }
+
+// AccountsSignIn -> An struct that describe the needed key field for logging the userIn
+type AccountsSignIn struct {
+	Email     	string	`json:"email" validate:"required,email"`
+	Password 	string	`json:"password" validate:"required"`
+}
