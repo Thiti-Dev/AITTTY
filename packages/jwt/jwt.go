e5d62ad580017004e212e59dcf9e791fb616d117
Thiti Dev <thiti.mwk.main@gmail.com>
2021-01-01 23:01:06 +0700

    :gem: [pkg->jwt]: Created (fn)GetSignedTokenFromData

---
+++ packages/jwt/jwt.go
@@ -0,0 +1,28 @@
+package jwt
+
+import (
+	"fmt"
+
+	"github.com/Thiti-Dev/AITTTY/models"
+	"github.com/dgrijalva/jwt-go"
+)
+
+// GetSignedTokenFromData -> is a (fn) that will sign a token with the given data
+func GetSignedTokenFromData(data models.RequiredDataToClaims) string{
+	claims := models.CustomClaims{
+		Username: data.Username,
+		Email: data.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: 15000,
+			Issuer:    "aittty.io",
+		},
+	}
+	// Create an unsigned token from the claims above
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Sign the token ->  preferably at least 256 bits in length (in-production xD)
+	signedToken, err := token.SignedString([]byte("secureSecretText"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	return signedToken
+}
